a79cce7ffb19b1ed01378c3f3e3b2701873fe511
Melisa Im <mim@adobe.com>
2020-03-13 23:47:22 -0700

    Done

--- Week 9/Assignment 6/app.js
+++ Week 9/Assignment 6/app.js
@@ -1,22 +1,26 @@
 
 const express = require('express');
 const mysql = require('./dbconfig.js');
-
+const bodyParser = require('body-parser');
 const app = express();
 const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.get('/reset-table',function(req,res,next){
 	var context = {};
-	mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
-	  var createString = "CREATE TABLE todo(" +
+	mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){
+	  var createString = "CREATE TABLE workouts (" +
 	  "id INT PRIMARY KEY AUTO_INCREMENT," +
 	  "name VARCHAR(255) NOT NULL," +
-	  "done BOOLEAN," +
-	  "due DATE)";
+	  "reps INT," +
+	  "weight INT," +
+	  "unit VARCHAR(255)," +
+	  "date DATE)";
 	  mysql.pool.query(createString, function(err){
 		context.results = "Table reset";
 		res.render('home',context);
@@ -24,43 +28,63 @@ app.get('/reset-table',function(req,res,next){
 	});
 });
 
-app.get('/insert',function(req,res,next){
-	var context = {};
-	mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
-	  if(err){
-		next(err);
-		return;
-	  }
-	  context.results = "Inserted id " + result.insertId;
-	  res.render('home',context);
-	});
-});
-
 app.get('/',function(req,res,next){
 	var context = {};
-	mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+	mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
 	  if(err){
 		next(err);
 		return;
 	  }
-	  context.results = JSON.stringify(rows);
+	  rows.map((row, index) => {
+		  row.number = index;
+		  let day = new Date(row.date);
+		  row.day = day.getUTCDate();
+		  row.month = day.getUTCMonth();
+		  row.year = day.getUTCFullYear();
+		  console.log("MAP MONTH", row.month);
+	  })
+	  context.results = rows;
 	  res.render('home', context);
 	});
 });
 
-app.get('/simple-update',function(req,res,next){
-	var context = {};
-	mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
-	  [req.query.name, req.query.done, req.query.due, req.query.id],
-	  function(err, result){
-	  if(err){
-		next(err);
-		return;
-	  }
-	  context.results = "Updated " + result.changedRows + " rows.";
-	  res.render('home',context);
-	});
+app.post('/', function (req,res,next) {
+	if (req.body.event == "delete") {
+		mysql.pool.query("DELETE FROM workouts WHERE id=?", [req.body.id], (err, result) => {
+			if(err) {
+				console.log("DELETE ERROR: ", err);
+				next(err);
+				return;
+			}
+		});
+	} else if (req.body.event == "edit") {
+		const date = `${req.body.year}-${req.body.month + 1}-${req.body.day}`;
+		mysql.pool.query("UPDATE workouts SET name=?, weight=?, reps=?, unit=?, date=? WHERE id=?",
+			  [req.body.name, req.body.weight, req.body.reps, req.body.unit, date, req.body.id],
+			  function(err, result){
+			  if(err){
+				next(err);
+				return;
+			  }
+			//   context.results = "Updated " + result.changedRows + " rows.";
+			  res.render('home',context);
+			});
+	} else {
+		const date = `${req.body.year}-${req.body.month}-${req.body.day}`;
+		console.log(date);
+		var context = {};
+		mysql.pool.query("INSERT INTO workouts (name, weight, reps, unit, date) VALUES (?,?,?,?,?)", [req.body.name, req.body.weight, req.body.reps, req.body.unit, date], function(err, result){
+		  if(err){
+			console.log("ERROR:", err);
+			next(err);
+			return;
+		  }
+		  context.results = "Inserted id " + result.insertId;
+		  res.render('home',context);
+		});
+	}
 });
+
 
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
--- Week 9/Assignment 6/dbconfig.js
+++ Week 9/Assignment 6/dbconfig.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
-  host  : 'localhost',
-  user  : 'student',
-  password: 'default',
-  database: 'student'
+  connectionLimit : 10, 
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs290_imm',
+  password        : '6z7PZ6Z$OdPeHGjf2ToH',
+  database        : 'cs290_imm'
 });
-
 module.exports.pool = pool;
